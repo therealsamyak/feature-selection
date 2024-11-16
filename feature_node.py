7be621550d766e4074b0985293d657612d38d9e1
KleeWai <ramvrao@gmail.com>
2024-11-16 15:06:25 -0800

    array implementation for next

--- feature_node.py
+++ feature_node.py
@@ -4,5 +4,5 @@ class Node:
     def __init__(self, features: list[int]):
         self.features = features  
         self.score = feature_validator(features) 
-        self.forward = None 
+        self.next = []
         self.backward = None 
