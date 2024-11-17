3a1dbd43adfd61493fb82f442a3190242c0a2b96
therealsamyak <therealsamyak@gmail.com>
2024-11-16 16:40:55 -0800

    added sort automatically

--- feature_node.py
+++ feature_node.py
@@ -1,8 +1,11 @@
-from validator import feature_validator  
+from validator import feature_validator
+
 
 class Node:
     def __init__(self, features: list[int]):
-        self.features = features  
-        self.score = feature_validator(features) 
+        self.features = features
+        self.score = feature_validator(features)
         self.next = []
-        self.backward = None 
+        self.backward = None
+
+        self.features.sort()
