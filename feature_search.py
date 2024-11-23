759de4b08b655d0cd593a163c7fcd9ce38c4bed3
therealsamyak <therealsamyak@gmail.com>
2024-11-22 16:47:59 -0800

    fixed backwards search * added backtracking

--- feature_search.py
+++ feature_search.py
@@ -4,10 +4,10 @@ from feature_node import Node
 class FeatureDriver:
     def __init__(self, num_features) -> None:
         self.num_features = num_features
-        self.global_node = Node([])
+        self.global_node = Node([], None)
 
     def forwards(self) -> Node:
-        self.global_node = Node([])
+        self.global_node = Node([], None)
         prev_node = self.global_node
 
         while len(prev_node.features) < self.num_features:
@@ -18,9 +18,12 @@ class FeatureDriver:
                     continue
 
                 new_features = sorted(prev_node.features + [index])
-                curr_node = Node(new_features)
+                curr_node = Node(new_features, prev_node)
+                print(
+                    f"Using feature(s) {curr_node.features}, the accuracy is {curr_node.score}"
+                )
 
-                if best_node is None or curr_node > best_node:
+                if not best_node or curr_node > best_node:
                     best_node = curr_node
 
             prev_node = best_node
@@ -43,44 +46,61 @@ class FeatureDriver:
                 print()
 
         return self.global_node
-    
+
     def backwards(self) -> Node:
+        self.global_node = Node(list(range(1, self.num_features + 1)), None)
+        print()
+        print(
+            f"Feature set {self.global_node.features} is initially the best, accuracy is {self.global_node.score}"
+        )
+        print()
+
         prev_node = self.global_node
 
-        while len(prev_node.features) > 1:
+        while len(prev_node.features) > 0:
             best_node = None
 
             for index in prev_node.features:
-                #basically removes an index
-                new_features = [f for f in prev_node.features if f != index] 
-                curr_node = Node(sorted(new_features))
+                new_features = [f for f in prev_node.features if f != index]
 
+                if len(new_features) <= 0:
+                    continue
+
+                curr_node = Node(sorted(new_features), prev_node)
+
+                if curr_node.features:
+                    print(
+                        f"Using feature(s) {curr_node.features}, the accuracy is {curr_node.score}"
+                    )
 
                 if best_node is None or curr_node > best_node:
                     best_node = curr_node
 
-            prev_node = best_node
+            if best_node and best_node.features:
+                prev_node = best_node
 
-            if self.global_node < best_node:
-                self.global_node = best_node
-                print()
-                print(
-                    f"Feature set {self.global_node.features} is a NEW best, accuracy is {self.global_node.score}"
-                )
-                print()
+                if self.global_node < best_node:
+                    self.global_node = best_node
+                    print()
+                    print(
+                        f"Feature set {self.global_node.features} is a NEW best, accuracy is {self.global_node.score}"
+                    )
+                    print()
+                else:
+                    print()
+                    print(
+                        f"Feature set {best_node.features} was best at this level, accuracy is {best_node.score}"
+                    )
+                    print(
+                        f"Feature set {self.global_node.features} is still the overall best, accuracy is {self.global_node.score}"
+                    )
+                    print()
             else:
-                print()
-                print(
-                    f"Feature set {best_node.features} was best at this level, accuracy is {best_node.score}"
-                )
-                print(
-                    f"Feature set {self.global_node.features} is still the overall best, accuracy is {self.global_node.score}"
-                )
-                print()
+                break
 
         return self.global_node
 
 
 test = FeatureDriver(4)
 best_node = test.forwards()
-print("best subset ", best_node.features, best_node.score)
+print("Finished search!! The best feature subset is", best_node.features, ", which has an accuracy of ", best_node.score)
