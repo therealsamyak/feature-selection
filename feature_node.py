8375b19a717ecb05fcbb6dd8b053fc2b6930cea1
therealsamyak <therealsamyak@gmail.com>
2024-11-22 16:47:36 -0800

    operator overloading now accounts for length

--- feature_node.py
+++ feature_node.py
@@ -4,31 +4,31 @@ from validator import feature_validator
 class Node:
     def __init__(self, features: list[int], prev=None):
         self.features = sorted(features)  # Automatically sort features
-        self.score = feature_validator(self.features)  # Validate features and compute score
+        self.score = feature_validator(
+            self.features
+        )  # Validate features and compute score
         self.next = []  # Initialize next as an empty list
         self.backward = prev  # Assign the backward link to the provided previous Node
 
     @classmethod
-    def from_existing(cls, features: list[int], prev: 'Node'):
+    def from_existing(cls, features: list[int], prev: "Node"):
         return cls(features, prev)
 
     def __lt__(self, other):
+        if self.score == other.score:
+            return len(self.features) > len(other.features)
         return self.score < other.score
 
-    def __le__(self, other):
-        return self.score <= other.score
-
     def __gt__(self, other):
+        if self.score == other.score:
+            return len(self.features) < len(other.features)
         return self.score > other.score
 
-    def __ge__(self, other):
-        return self.score >= other.score
-
     def __eq__(self, other):
-        return self.score == other.score
+        return self.score == other.score and len(self.features) == len(other.features)
 
     def __ne__(self, other):
-        return self.score != other.score
+        return not self.__eq__(other)
 
     def __repr__(self):
         return f"Node(features={self.features}, score={self.score})"
