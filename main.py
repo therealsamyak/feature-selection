754a07948932981fcb4fad7ff3859a5cd9c197d4
therealsamyak <therealsamyak@gmail.com>
2024-11-22 16:59:21 -0800

    added main

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 # import stuff to main as necessary
+from feature_search import FeatureDriver
 
 
 def main():
-    return 0
+    print("Welcome to Samyak & Ram's Feature Selection Algorithm.")
+
+    total_features = int(input("Please enter total number of features: "))
+    if total_features <= 0:
+        raise ValueError("The number of features must be a positive integer.")
+
+    print()
+    print("Type the number of the algorithm you want to run.")
+    print("1. Forward Selection")
+    print("2. Backward Elimination")
+
+    choice = int(input("Your choice: "))
+    if choice not in (1, 2):
+        raise ValueError("Please choose a valid option (1 or 2).")
+
+    test = FeatureDriver(total_features)
+
+    if choice == 1:
+        best_node = test.forwards()
+        print(
+            "\nFinished search!! The best feature subset is",
+            best_node.features,
+            ", which has an accuracy of",
+            best_node.score,
+        )
+    elif choice == 2:
+        best_node = test.backwards()
+        print(
+            "\nFinished search!! The best feature subset is",
+            best_node.features,
+            ", which has an accuracy of",
+            best_node.score,
+        )
 
 
 if __name__ == "__main__":
