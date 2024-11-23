ec18fa07fba2c2964039bdecbf71b920e8c99977
therealsamyak <therealsamyak@gmail.com>
2024-11-22 16:48:09 -0800

    removed print statement from validation function

--- validator.py
+++ validator.py
@@ -7,7 +7,6 @@ def feature_validator(features: list[int]) -> int:
     if len(features) <= 0:
         return 0
     accuracy = random.randint(1, 100)
-    print(f"Using feature(s) {features}, the accuracy is {accuracy}")
     return accuracy
 
 
