ea95d43a658a138ff7a6d127a1cfdace6b0260a2
therealsamyak <therealsamyak@gmail.com>
2024-12-06 15:21:45 -0800

    finished classify method

--- classifier.py
+++ classifier.py
@@ -1,10 +1,37 @@
+from math import sqrt
+
+
 class Classifier:
-    def __init__(self, node_map):
-        self.node_map = node_map
+    def __init__(self, global_node_map):
+        self.global_node_map = global_node_map
         self.local_node_map = None
+        self.feature_subset_array = None
 
     def classify(self, uniqueID):
-        pass
+
+        if not self.local_node_map or not self.feature_subset_array:
+            raise ValueError(
+                "Model is not trained yet. Please train the model before continuing."
+            )
+
+        target_node = self.global_node_map[uniqueID]
+        target_features = [target_node.features[i] for i in self.feature_subset_array]
+
+        global_dist = float("inf")
+        closest_node = None
+
+        for nodeID, nodeData in self.local_node_map.items():
+            local_dist = sqrt(
+                (target - local) ** 2
+                for target, local in zip(target_features, nodeData.features)
+            )
+
+            if local_dist < global_dist:
+                global_dist = local_dist
+                closest_node = nodeID
+
+        return self.local_node_map[closest_node].label
 
     def train(self, uniqueIDArray, featureSubsetArray):
+        self.feature_subset_array = sorted(featureSubsetArray)
         pass
