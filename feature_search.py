15c527e92f430b62eddbe62263e6bb21a73021f4
therealsamyak <therealsamyak@gmail.com>
2024-12-06 16:57:31 -0800

    moved part1 driver here from main

--- feature_search.py
+++ feature_search.py
@@ -98,3 +98,39 @@ class FeatureDriver:
                 print()
 
         return self.global_node
+
+    def userInputDriver(self):
+        print()
+        print("Welcome to Samyak & Ram's Feature Selection Algorithm.")
+
+        total_features = int(input("Please enter total number of features: "))
+        if total_features <= 0:
+            raise ValueError("The number of features must be a positive integer.")
+
+        print()
+        print("Type the number of the algorithm you want to run.")
+        print("1. Forward Selection")
+        print("2. Backward Elimination")
+
+        choice = int(input("Your choice: "))
+        if choice not in (1, 2):
+            raise ValueError("Please choose a valid option (1 or 2).")
+
+        test = FeatureDriver(total_features)
+
+        if choice == 1:
+            best_node = test.forwards()
+            print(
+                "\nFinished search!! The best feature subset is",
+                best_node.features,
+                ", which has an accuracy of",
+                best_node.score,
+            )
+        elif choice == 2:
+            best_node = test.backwards()
+            print(
+                "\nFinished search!! The best feature subset is",
+                best_node.features,
+                ", which has an accuracy of",
+                best_node.score,
+            )
