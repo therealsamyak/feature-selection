adb21c0a150036ce5c76f1125083bc7b57421609
therealsamyak <therealsamyak@gmail.com>
2024-12-06 23:39:56 -0800

    fixed main

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ def main():
     # FeatureDriver.userInputDriver()
 
     # Part 2
+
+    # small data set
     # dataset_path = "datasets/small-test-dataset.txt"
     # test_validator = Validator(dataset_path)
     # print(test_validator.validate([3, 5, 7]))
 
-    # Debug
+    # large data set
     dataset_path = "datasets/small-test-dataset.txt"
     test_validator = Validator(dataset_path)
     test_validator.validate([1, 15, 27])
