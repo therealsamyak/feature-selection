cd2613cd10236c1047327fc7c061630649206453
therealsamyak <therealsamyak@gmail.com>
2024-12-06 16:32:35 -0800

    updated validation function

--- validator.py
+++ validator.py
@@ -1,8 +1,26 @@
-import random
-
-random.seed(10)
+from classifier import *
 
 
-def feature_validator(features: list[int]) -> int:
-    accuracy = random.randint(1, 100)
-    return accuracy
+class Validator:
+    def __init__(self, file_name: str):
+        self.classifier: Classifier = Classifier(file_name)
+        self.global_map: Dict[int, Item_Node] = Classifier.global_node_map
+
+    def validate(self, feature_subset_array: List[int]):
+        """
+        Calculates accuracy of feature_subset using Leave-One-Out validation
+        """
+
+        keys = list(self.global_map.keys())
+        correctly_classified = 0
+
+        # leave one out validator
+        for index, uniqueID in enumerate(keys):
+            training_data = keys[:index] + keys[index + 1 :]
+
+            self.classifier.train(training_data, feature_subset_array)
+
+            if self.global_map[uniqueID].label == self.classifier.classify(uniqueID):
+                correctly_classified += 1
+
+        return correctly_classified * 1.0 / len(keys)
