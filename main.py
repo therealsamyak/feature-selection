5e8cc55492b1f80016ca9a366424f1664c68ed93
therealsamyak <therealsamyak@gmail.com>
2024-12-06 20:38:26 -0800

    Revert "numberofcolomns func test"
    This reverts commit 904e3e3ebe7f61bed925423ae687ef973440989f.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # import stuff to main as necessary
 from src import *
 
+
 def main():
     # Part 1
     # FeatureDriver.userInputDriver()
@@ -14,5 +15,4 @@ def main():
 
 
 if __name__ == "__main__":
-    classifier = Classifier("willremovelater.txt")
-    print(classifier.get_number_of_columns)
+    main()
